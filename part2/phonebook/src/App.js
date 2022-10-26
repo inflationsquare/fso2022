186927c557e77c5bcae87313704578579e311bb4
Stephen Burke <stephen@recommenderx.com>
2022-10-26 19:54:40 +0100

    exercise 2.10

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,6 +2,54 @@ import { useState } from 'react'
 
 const Contact = ({name, number}) => <><li key={name}>{name} {number}</li></>
 
+const PersonForm = ({name, number, persons, nameUpdate, numberUpdate, personsUpdate}) => {
+  const updateName = (event) => {
+    nameUpdate(event.target.value)
+  }
+
+  const updateNumber = (event) => {
+    numberUpdate(event.target.value)
+  }
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const person = {name: name, number: number}
+    persons.filter(p => p.name === name).length > 0 ? 
+      alert(`${name} is already added to phonebook`) :
+      personsUpdate(persons.concat(person))
+  }
+
+  return(
+    <form onSubmit={addPerson}>
+        <h2>add a new</h2>
+        <div>
+          name: <input value={name} onChange={updateName}/>
+        </div>
+        <div>
+          number: <input value={number} onChange={updateNumber}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+    </form>)
+}
+
+const Numbers = ({persons, filterValue}) => {return(<>
+      <h2>Numbers</h2>
+      <ul>
+    {persons.filter(p => p.name.toLowerCase().includes(filterValue)).map(p => <Contact name={p.name} number={p.number}/>)}
+      </ul>
+  </>)
+}
+
+const Filter = ({filterValue, filterUpdate}) => {
+  const updateFilter = (event) => {
+    filterUpdate(event.target.value)
+  }
+
+  return(<div>filter shown with: <input value={filterValue} onChange={updateFilter}/></div>)
+}
+
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas', number: '12345678'},
@@ -13,47 +61,12 @@ const App = () => {
   const [newNumber, setNewNumber] = useState('')
   const [filterValue, setFilter] = useState('')
 
-  const addPerson = (event) => {
-    event.preventDefault()
-    const person = {name: newName, number: newNumber}
-    persons.filter(p => p.name === newName).length > 0 ? 
-      alert(`${newName} is already added to phonebook`) :
-      setPersons(persons.concat(person))
-  }
-
-  const updateName = (event) => {
-    setNewName(event.target.value)
-  }
-
-  const updateNumber = (event) => {
-    setNewNumber(event.target.value)
-  }
-
-  const updateFilter = (event) => {
-    setFilter(event.target.value)
-  }
-
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>filter shown with: <input value={filterValue} onChange={updateFilter}/></div>
-      <form onSubmit={addPerson}>
-        <h2>add a new</h2>
-        <div>
-          name: <input value={newName} onChange={updateName}/>
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={updateNumber}/>
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      <ul>
-    {persons.filter(p => p.name.toLowerCase().includes(filterValue)).map(p => <Contact name={p.name} number={p.number}/>)}
-      </ul>
-      
+      <Filter filterValue={filterValue} filterUpdate={setFilter}/>
+      <PersonForm name={newName} number={newNumber} persons={persons} nameUpdate={setNewName} numberUpdate={setNewNumber} personsUpdate={setPersons}  />
+    <Numbers persons={persons} filterValue={filterValue} />
     </div>
   )
 }
