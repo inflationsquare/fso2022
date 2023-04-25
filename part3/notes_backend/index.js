015c582ad15fa13b03b236387b40beeff1003f54
Stephen Burke <burkes14@tcd.ie>
2023-04-25 20:54:46 +0100

    Part 3a

---
+++ part3/notes_backend/index.js
@@ -0,0 +1,69 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+let notes = [{
+    id: 1,
+    content: "HTML is easy",
+    important: true
+  },
+  {
+    id: 2,
+    content: "Browser can execute only JavaScript",
+    important: false
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true
+  }] 
+
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/notes', (request, response) => {
+    response.json(notes)
+})
+
+app.get('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const note = notes.find(note => note.id === id)
+    note ? response.json(note) : response.status(404).end()
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    notes = notes.filter(note => note.id !== id)
+    response.status(204).end()
+})
+
+const generateId = () => {
+    const maxId = notes.length > 0 ? Math.max(...notes.map(x => x.id)) : 0
+    return maxId + 1 
+}
+
+app.post('/api/notes', (request, response) => {
+
+    const body = request.body
+    if (!body.content) {
+        return response.status(400).json({"error": "Missing content"})
+    }
+    const note = {
+        content: body.content,
+        important: body.important || false,
+        id: generateId()
+    }
+
+    console.log(note)
+
+    notes = notes.concat(note)
+    response.json(note)
+})
+
+
+const PORT = 3001
+app.listen(PORT, () => {
+console.log(`server running on port ${PORT}`)
+})
